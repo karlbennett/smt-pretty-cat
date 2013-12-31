02539a3bac3cedc1f6454c17f157fd9d300f940e
Karl Bennett <karlbennett@orcon.net.nz>
2014-01-01 00:03:03 +1300

    Removed the keyword assertions from the JavaColourConfigurationTests.

--- src/test/java/shiver/me/timbers/JavaColourConfigurationTest.java
+++ src/test/java/shiver/me/timbers/JavaColourConfigurationTest.java
@@ -2,20 +2,16 @@ package shiver.me.timbers;
 
 import org.junit.Test;
 import shiver.me.timbers.transform.java.rules.Annotation;
-import shiver.me.timbers.transform.java.types.Abstract;
-import shiver.me.timbers.transform.java.types.Catch;
 import shiver.me.timbers.transform.java.types.Comment;
 import shiver.me.timbers.transform.java.types.IntegerLiteral;
 import shiver.me.timbers.transform.java.types.JavaDoc;
 import shiver.me.timbers.transform.java.types.StringLiteral;
-import shiver.me.timbers.transform.java.types.This;
-import shiver.me.timbers.transform.java.types.Try;
 
-import static java.util.AbstractMap.SimpleEntry;
 import java.util.Collection;
 import java.util.HashSet;
 
-import static java.util.Map.*;
+import static java.util.AbstractMap.SimpleEntry;
+import static java.util.Map.Entry;
 import static org.junit.Assert.assertEquals;
 import static shiver.me.timbers.FOREGROUND_COLOUR.BLUE;
 import static shiver.me.timbers.FOREGROUND_COLOUR.BRIGHT_BLACK;
@@ -23,8 +19,7 @@ import static shiver.me.timbers.FOREGROUND_COLOUR.BRIGHT_WHITE;
 import static shiver.me.timbers.FOREGROUND_COLOUR.GREEN;
 import static shiver.me.timbers.FOREGROUND_COLOUR.RED;
 import static shiver.me.timbers.FOREGROUND_COLOUR.WHITE;
-import static shiver.me.timbers.FOREGROUND_COLOUR.YELLOW;
-import static shiver.me.timbers.checks.Checks.*;
+import static shiver.me.timbers.checks.Checks.isNotNull;
 
 public class JavaColourConfigurationTest {
 
@@ -34,11 +29,6 @@ public class JavaColourConfigurationTest {
     public void testGetForegroundColourName() {
 
         final ColourConfiguration configuration = new JavaColourConfiguration();
-
-        assertEquals("\"abstract\" colour should be correct.", YELLOW,
-                configuration.getForegroundColourName(Abstract.class));
-        assertEquals("\"catch\" colour should be correct.", YELLOW, configuration.getForegroundColourName(Catch.class));
-        assertEquals("\"this\" colour should be correct.", YELLOW, configuration.getForegroundColourName(This.class));
 
         assertEquals("\"JavaDoc\" colour should be correct.", GREEN,
                 configuration.getForegroundColourName(JavaDoc.class));
@@ -53,25 +43,25 @@ public class JavaColourConfigurationTest {
     @Test
     public void testGetForegroundColourNameWithSetSystemProperties() {
 
-        final String tryPropertyName = "java." + Try.class.getSimpleName();
+        final String annotationPropertyName = "java." + Annotation.class.getSimpleName();
         final String stringLiteralPropertyName = "java." + StringLiteral.class.getSimpleName();
 
-        recordProperties(tryPropertyName, stringLiteralPropertyName);
+        recordProperties(annotationPropertyName, stringLiteralPropertyName);
 
-        System.setProperty(tryPropertyName, "BRIGHT_BLACK");
+        System.setProperty(annotationPropertyName, "BRIGHT_BLACK");
         System.setProperty(stringLiteralPropertyName, "BRIGHT_WHITE");
 
         final ColourConfiguration configuration = new JavaColourConfiguration();
 
-        assertEquals("\"try\" colour should be correct.", BRIGHT_BLACK,
-                configuration.getForegroundColourName(Try.class));
+        assertEquals("\"Annotation\" colour should be correct.", BRIGHT_BLACK,
+                configuration.getForegroundColourName(Annotation.class));
         assertEquals("\"StringLiteral\" colour should be correct.", BRIGHT_WHITE,
                 configuration.getForegroundColourName(StringLiteral.class));
 
         restoreProperties();
     }
 
-    private void recordProperties(String ...names) {
+    private void recordProperties(String... names) {
 
         properties = new HashSet<Entry<String, String>>(names.length);
 
