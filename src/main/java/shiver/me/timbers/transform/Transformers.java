99b86a8445daa82360059516bb41701937f0ace6
Karl Bennett <karlbennett@orcon.net.nz>
2014-01-03 10:39:11 +1300

    Moved the Transformers interface up into the smt-transform project.

--- src/main/java/shiver/me/timbers/transform/Transformers.java
+++
@@ -1,7 +0,0 @@
-package shiver.me.timbers.transform;
-
-/**
- * A container of {@link shiver.me.timbers.transform.Transformation}s.
- */
-public interface Transformers<K, T extends Transformation> extends Container<K, CompositeTransformer<T>> {
-}
