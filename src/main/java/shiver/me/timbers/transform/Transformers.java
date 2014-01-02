f133f79f533c262739a14e74c3b30919039c0830
Karl Bennett <karlbennett@orcon.net.nz>
2014-01-03 10:37:00 +1300

    Fixed typo in comment.

--- src/main/java/shiver/me/timbers/transform/Transformers.java
+++ src/main/java/shiver/me/timbers/transform/Transformers.java
@@ -1,7 +1,7 @@
 package shiver.me.timbers.transform;
 
 /**
- * An container of {@link shiver.me.timbers.transform.Transformation}s.
+ * A container of {@link shiver.me.timbers.transform.Transformation}s.
  */
 public interface Transformers<K, T extends Transformation> extends Container<K, CompositeTransformer<T>> {
 }
