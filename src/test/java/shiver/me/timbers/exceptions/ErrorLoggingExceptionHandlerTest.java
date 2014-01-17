0494e77a9e6f7689f352c695f31a1d8096084095
karlbennett <where_1ts@yahoo.com>
2014-01-17 23:35:21 +1300

    Changed the ErrorLoggingExceptionHandlerTest to use a ByteArrayOutputStream instead of the sun internal ByteOutputStream.

--- src/test/java/shiver/me/timbers/exceptions/ErrorLoggingExceptionHandlerTest.java
+++ src/test/java/shiver/me/timbers/exceptions/ErrorLoggingExceptionHandlerTest.java
@@ -1,10 +1,10 @@
 package shiver.me.timbers.exceptions;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
@@ -12,14 +12,14 @@ import static org.junit.Assert.assertEquals;
 public class ErrorLoggingExceptionHandlerTest {
 
     private PrintStream oldErr;
-    private ByteOutputStream err;
+    private ByteArrayOutputStream err;
 
     @Before
     public void replaceStandardError() {
 
         oldErr = System.err;
 
-        System.setErr(new PrintStream(err = new ByteOutputStream()));
+        System.setErr(new PrintStream(err = new ByteArrayOutputStream()));
     }
 
     @After
